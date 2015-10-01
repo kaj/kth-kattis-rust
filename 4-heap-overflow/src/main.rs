7237c07c60207c00a3831fa97f9b1649a9e48614
Rasmus Kaj <kaj@kth.se>
2015-10-01 19:47:41 +0200

    Fourth submission.

---
+++ 4-heap-overflow/src/main.rs
@@ -0,0 +1,11 @@
+use std::collections::BTreeMap;
+
+/// Note: It is probably a good idea to set a smalish memory limit
+/// before running this ...
+fn main() {
+    let mut data = BTreeMap::new();
+    for i in 0..100000 {
+        data.insert(i, vec!(17; i));
+    }
+    println!("Ok!?!");
+}

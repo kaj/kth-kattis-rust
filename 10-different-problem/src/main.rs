901c9b23dea77911e168d783ffd2af9e999091eb
Rasmus Kaj <kaj@kth.se>
2016-08-25 00:23:05 +0200

    Maybe a cleaner implementation.

--- 10-different-problem/src/main.rs
+++ 10-different-problem/src/main.rs
@@ -1,20 +1,22 @@
+//! Computes the difference between non-negative integers.
+//! Each line of the input consists of a pair of integers.
+//! Each integer is between 0 and 1015 (inclusive).
+//! The input is terminated by end of file.
 use std::io;
 
-fn read_numbers() -> Option<(i64, i64)> {
+/// Read a line from standard input and parse it to a vec of numbers.
+/// Panics if there are non-numbers in the line.
+fn read_numbers() -> Option<Vec<i64>> {
     let mut line = String::new();
-    if let Ok(_) = io::stdin().read_line(&mut line) {
-        let v : Vec<i64> = line.split_whitespace()
-            .map(|s| s.parse::<i64>().unwrap())
-            .collect();
-        if v.len() == 2 {
-            return Some((v[0], v[1]))
-        }
-    }
-    None
+    io::stdin().read_line(&mut line).ok()
+        .map(|_| line.split_whitespace().map(|s| s.parse().unwrap()).collect())
 }
 
 fn main() {
-    while let Some((a, b)) = read_numbers() {
-        println!("{}", (a-b).abs());
+    while let Some(v) = read_numbers() {
+        if v.len() != 2 {
+            break;
+        }
+        println!("{}", (v[0] - v[1]).abs());
     }
 }

ca6cec85c190f16d65de677ad91f2a3edb898d45
Rasmus Kaj <kaj@kth.se>
2018-08-22 00:59:45 +0200

    Maybe a nicer solution.

--- 10-different-problem/src/main.rs
+++ 10-different-problem/src/main.rs
@@ -6,17 +6,14 @@ use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
-    let lines = stdin.lock()
-                     .lines()
-                     .map(|line| line.ok().and_then(line2numbers))
-                     .take_while(|l| l.is_some())
-                     .map(|l| l.unwrap());
-
-    for v in lines.take_while(|v| v.len() == 2) {
-        println!("{}", (v[0] - v[1]).abs());
+    for line in stdin.lock().lines() {
+        if let Some((a, b)) = line.ok().and_then(|line| line2numbers(&line)) {
+            println!("{}", (a - b).abs());
+        }
     }
 }
 
-fn line2numbers(line: String) -> Option<Vec<i64>> {
-    line.split_whitespace().map(|s| s.parse().ok()).collect()
+fn line2numbers(line: &str) -> Option<(i64, i64)> {
+    let mut iter = line.split_whitespace().filter_map(|s| s.parse().ok());
+    iter.next().and_then(|a| iter.next().map(|b| (a, b)))
 }
